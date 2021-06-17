4d380c03d31cc5df7114f017c05ed0c830ead915
brecert <me@bree.dev>
2021-06-17 10:47:25 -0400

    restructure project
    - remove dependency on denoify
      - deno imports are temporarily broken

--- test.ts
+++ test.ts
@@ -1,8 +1,5 @@
-import {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.79.0/testing/asserts.ts";
-import { seededColor } from "../deno_pkg/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
+import { seededColor } from "./mod.ts";
 
 Deno.test("[seededColor] example", () => {
   assertEquals(seededColor("bree"), "#13fa4e");
@@ -11,6 +8,6 @@ Deno.test("[seededColor] example", () => {
 
   assertEquals(
     seededColor(213021939013239301n),
-    "#730de5"
+    "#730de5",
   );
 });
